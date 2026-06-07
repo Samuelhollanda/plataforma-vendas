d569b8241bdef3ed21360d3b7ca339ef842dd9d9
Samuelhollanda <samuelholandadev@outlook.com>
2026-06-07 12:45:45 -0300

    adcionando service de transacao de vendas

---
+++ backend/src/services/VendaService.ts
@@ -0,0 +1,41 @@
+import { NetworkResources } from "node:inspector/promises";
+import prisma from "../config/prisma";
+
+export class VendaService {
+    async realizarVenda(produtoId: string, quantidadeVendida: number, valorTotal: number) {
+        const produto = await prisma.produto.findUnique({
+            where: { id: produtoId },
+        });
+
+        if (!produto) {
+            throw new Error('Produto não encontrado');
+        }
+
+        if (produto.quantidade_estoque < quantidadeVendida) {
+            throw new Error('Estoque insuficiente para realizar esta venda.');
+        }
+
+        const resultado = await prisma.$transaction([
+            //cria o registro da venda
+            prisma.venda.create({
+                data: {
+                    quantidade: quantidadeVendida,
+                    valorTotal: valorTotal,
+                    produtoId: produtoId,
+                },
+            }),
+
+            //diminui o estoque do produto
+            prisma.produto.update({
+                where: { id: produtoId },
+                data: {
+                    quantidade_estoque: {
+                        decrement: quantidadeVendida,
+                    },
+                },
+            }),
+        ]);
+
+        return resultado[0];
+    }
+}
